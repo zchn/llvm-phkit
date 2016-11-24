6d0f607baa54b5b3ebf1725cddfff28ba431eacb
Kevin Chen <kevin.zchn@gmail.com>
2016-11-24 00:58:14 -0500

    snapshotting

--- test/Phkit/SoftBoundTransformSpec.hs
+++ test/Phkit/SoftBoundTransformSpec.hs
@@ -1,14 +1,43 @@
 module Phkit.SoftBoundTransformSpec (spec) where
 
-import Test.Hspec (describe, it, shouldBe, shouldContain, Spec)
+import qualified LLVM.General.AST as LGA
+
+import Test.Hspec (describe, it, pendingWith, shouldBe, shouldContain, Spec, Expectation)
 import Phkit.IO
 import Phkit.SoftBoundTransform
 
+forEachInsn :: LGA.Module ->
+  (Either LGA.Instruction LGA.Terminator -> Maybe Expectation) -> Expectation
+forEachInsn LGA.Module{LGA.moduleDefinitions = defs} pred =
+  let expectations = concat $ map processDef defs in
+  case expectations of
+    [] -> 1 `shouldBe` 1
+    h:_ -> h
+  where processDef (LGA.GlobalDefinition (LGA.Function{LGA.basicBlocks = bb}))  =
+          concat $ map (processBb pred)
+        processBb (LGA.BasicBlock _ nInsns nTerm) =
+          (concat $ map (processInsn . deName) nInsns) ++
+          processTerm (deName nTerm)
+        processInsn insn = pred (Left insn)
+        processTerm term = pred (Right term)
+
+deName :: LGA.Named A -> A
+deName (LGA.Do a) = a
+deName (_ LGA.:= a) = a
+
+
 spec :: Spec
-spec = 
+spec =
     describe "SoftBound transform tests" $
-    do it "inserts sbcheck and sbsave." $
-           do modu <- 
+    do it "names a meta for each def" $
+         do modu <-
+              withModuleFromPathIO
+              "test/testdata/interpret-indirectbr.c"
+              return
+            let newMod = softBoundRewriteResultOf modu
+       it "inserts sbcheck and sbsave." $
+           do pendingWith "Not implemented."
+              modu <-
                   withModuleFromPathIO
                       "test/testdata/interpret-indirectbr.c"
                       return
@@ -20,7 +49,8 @@ spec =
               newModString `shouldContain`
                   "define internal i32 @interpret_threaded(i8* %opcodes) #1 {"
        it "inserts sbload." $
-           do modu <- 
+           do pendingWith "Not implemented."
+              modu <-
                   withModuleFromPathIO
                       "test/testdata/interpret-indirectbr.c"
                       return
@@ -31,7 +61,7 @@ spec =
               newModString `shouldContain`
                   "define internal i32 @interpret_threaded(i8* %opcodes) #1 {"
        it "inserts sbfunc." $
-           do modu <- 
+           do modu <-
                   withModuleFromPathIO
                       "test/testdata/interpret-indirectbr.c"
                       return
